2a76cf7318c52abdc09dd1c28a341c0a582ae7d1
Isaac W Hanson <isaac@starlig.ht>
2017-03-18 16:00:03 -0400

    start menu shortcut

--- CYGWIN-PATCHES/postinstall.sh
+++ CYGWIN-PATCHES/postinstall.sh
@@ -1,2 +1,5 @@
 
 mkshortcut.exe -d 'Swan Console' -i /BlackSwan.ico -n 'Swan Console' -a '-i /BlackSwan.ico -' -D /bin/mintty
+
+mkshortcut.exe -d 'Swan Console' -i /BlackSwan.ico -n 'Swan Console' -a '-i /BlackSwan.ico -' -P /bin/mintty
+
