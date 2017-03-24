f7daa3f0f2c8ffc19bfb96f9544c70a4c06a6f93
Isaac W Hanson <isaac@starlig.ht>
2017-03-24 19:40:07 -0400

    zsh-compatible ssh-agent trap

--- etc/profile.d/ssh-agent.sh
+++ etc/profile.d/ssh-agent.sh
@@ -5,11 +5,16 @@
 function _start_agent {
     # spawn ssh-agent
     eval `ssh-agent` >/dev/null
-    trap 'eval `ssh-agent -k > /dev/null`'  EXIT
 }
 
 # Source SSH settings, if applicable
 if find "$HOME/.ssh/" -name 'id_*' 2>/dev/null >&2; then
-    _start_agent
+  _start_agent
+  if [ "$ZSH_VERSION" ]
+  then
+    zshexit() { eval `ssh-agent -k >/dev/null`; }  # zsh specific
+  else
+    trap 'eval `ssh-agent -k >/dev/null`' EXIT     # POSIX
+  fi
 fi
 
