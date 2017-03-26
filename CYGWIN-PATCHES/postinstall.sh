8c8e9ecba9789617ddacf2d95839ccef3b8dfd8f
Isaac W Hanson <isaac@starlig.ht>
2017-03-26 16:36:21 -0400

    rm old shtct

--- CYGWIN-PATCHES/postinstall.sh
+++ CYGWIN-PATCHES/postinstall.sh
@@ -1,5 +1,7 @@
-
+# desktop shortcut
 mkshortcut.exe -d 'Swan Console' -i /BlackSwan.ico -n 'Swan Console' -a '-i /BlackSwan.ico -' -D /bin/mintty
-
+# start menu shortcut
 mkshortcut.exe -d 'Swan Console' -i /BlackSwan.ico -n 'Swan/Swan Console' -a '-i /BlackSwan.ico -' -P /bin/mintty
+# rm old uninstallable shortcut
+rm -f "$(cygpath -P)/Swan Console.lnk"
 
